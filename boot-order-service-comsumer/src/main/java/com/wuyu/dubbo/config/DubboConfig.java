bcb26a300d1cc47d87408dc067dabcc481a4a041
wuyu <1308638159@qq.com>
2021-05-26 23:47:20 +0800

    fix

---
+++ boot-order-service-comsumer/src/main/java/com/wuyu/dubbo/config/DubboConfig.java
@@ -0,0 +1,41 @@
+package com.wuyu.dubbo.config;
+
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.ConsumerConfig;
+import com.alibaba.dubbo.config.MonitorConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DubboConfig {
+
+    @Bean
+    public ApplicationConfig applicationConfig() {
+        ApplicationConfig applicationConfig = new ApplicationConfig();
+        applicationConfig.setName("boot-order-service-comsumer");
+        return applicationConfig;
+    }
+
+    @Bean
+    public RegistryConfig registryConfig() {
+        RegistryConfig registryConfig = new RegistryConfig();
+        registryConfig.setProtocol("zookeeper");
+        registryConfig.setAddress("127.0.0.1:2181");
+        return registryConfig;
+    }
+
+    @Bean
+    public MonitorConfig monitorConfig() {
+        MonitorConfig monitorConfig = new MonitorConfig();
+        monitorConfig.setProtocol("registry");
+        return monitorConfig;
+    }
+
+    @Bean
+    public ConsumerConfig consumerConfig() {
+        ConsumerConfig consumerConfig = new ConsumerConfig();
+        consumerConfig.setCheck(false);
+        return consumerConfig;
+    }
+}
---
+++ boot-user-service-provider/src/main/java/com/wuyu/dubbo/config/DubboConfig.java
@@ -0,0 +1,43 @@
+package com.wuyu.dubbo.config;
+
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.MonitorConfig;
+import com.alibaba.dubbo.config.ProtocolConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DubboConfig {
+    @Bean
+    public ApplicationConfig applicationConfig() {
+        ApplicationConfig applicationConfig = new ApplicationConfig();
+        applicationConfig.setName("boot-user-service-provider");
+        return applicationConfig;
+    }
+
+    //<dubbo:registry protocol="zookeeper" address="127.0.0.1:2181"></dubbo:registry>
+    @Bean
+    public RegistryConfig registryConfig() {
+        RegistryConfig registryConfig = new RegistryConfig();
+        registryConfig.setProtocol("zookeeper");
+        registryConfig.setAddress("127.0.0.1:2181");
+        return registryConfig;
+    }
+
+    //<dubbo:protocol name="dubbo" port="20882"></dubbo:protocol>
+    @Bean
+    public ProtocolConfig protocolConfig() {
+        ProtocolConfig protocolConfig = new ProtocolConfig();
+        protocolConfig.setName("dubbo");
+        protocolConfig.setPort(20882);
+        return protocolConfig;
+    }
+
+    @Bean
+    public MonitorConfig monitorConfig() {
+        MonitorConfig monitorConfig = new MonitorConfig();
+        monitorConfig.setProtocol("registry");
+        return monitorConfig;
+    }
+}
